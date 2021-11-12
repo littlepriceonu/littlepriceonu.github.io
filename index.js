5bbd636027d6bf3411609481313c68f497dc23c6
littlepriceonu <littlepriceonu@icloud.com>
2021-11-12 15:48:29 -0800

    fixed stuff

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-setTimeout(() => {document.body.style ="overflow-y: visible;"}, 4900)
 scroll(0, 0)
+setTimeout(() => {document.body.style ="overflow-y: visible;"}, 4900)
+
+setInterval(() => {
+    if (innerWidth < 1034){
+        document.getElementById("programmingimage").className = "programmingimage2"
+    }
+    else {
+        //change back
+    }
+}, 10)
