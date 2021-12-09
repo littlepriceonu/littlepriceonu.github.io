8b159fd3b2812589690261bb90d77a0f9494bd33
littlepriceonu <littlepriceonu@icloud.com>
2021-12-08 19:32:49 -0800

    Fixed Mobile Look

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ scroll(0, 0)
 setTimeout(() => {document.body.style ="overflow-y: visible;"}, 4900)
 
 setInterval(() => {
-    if (innerWidth < 1034){
+    if (innerWidth <= 1790){
         document.getElementById("programmingimage").className = "programmingimage2"
+        document.getElementById("wrapper3text").className = "wrapper3textmobile font25"
+        document.getElementById("wrapper2").className = "wrapper2 font25"
+        document.getElementById("wrapper").className = "wrapper font25"
     }
     else {
-        //change back
+        document.getElementById("programmingimage").className = "programmingimage"
+        document.getElementById("wrapper3text").className = "wrapper3text"
+        document.getElementById("wrapper2").className = "wrapper2"
+        document.getElementById("wrapper").className = "wrapper"
     }
 }, 10)
