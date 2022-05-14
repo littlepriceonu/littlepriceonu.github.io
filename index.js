40a26aa071ca03029a0979907dfcdf388176291d
littlepriceonu <littlepriceonu@icloud.com>
2022-05-14 11:22:18 -0700

    Added Name And PFP (Animation In Dev)

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
+class ObserverFunction {
+    constructor(element, callback, falseCallback) {
+        this.element = element;
+        this.callback = callback;
+        this.falseCallback = falseCallback;
+    }
+
+    serialize() {
+        return({
+            falseCallback: this.falseCallback,
+            callback: this.callback,
+            element: this.element
+        })
+    }
+}
+
+class Observer {
+    constructor(ObserverFunctions, updateSpeed) {
+        this.updateSpeed = updateSpeed
+        if (!ObserverFunctions) this.ObserverFunctions = [];
+        else this.ObserverFunctions = ObserverFunctions;
+        this.startLoop()
+    }
+
+    startLoop() {
+        this.id = setInterval(this.Loop.bind(this), this.updateSpeed)
+    }
+
+    Loop() {
+        for(let i = 0; i < this.ObserverFunctions.length; i++){
+            let Func = this.ObserverFunctions[i]
+            if (this.isInView(Func.serialize().element)) {
+                Func.serialize().callback()
+            }
+            else {
+                Func.serialize().falseCallback()
+            }
+        }
+    }
+
+    addObserverFunction(ObserverFunctionn) {
+        this.ObserverFunctions.push(ObserverFunctionn)
+    }
+
+    removeObserverFunction(ObserverFunctionn) {
+        this.ObserverFunctions.splice(this.ObserverFunctions.indexOf(ObserverFunctionn), 1)
+    }
+
+    isInView(element) {
+        const rect = element.getBoundingClientRect();
+        return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth);
+    }
+}
+
 console.log(document.querySelector("#wrapper").getBoundingClientRect().top)
+
+var allowMobileScroll = false
 
 setTimeout( () => {
     if (document.querySelector("#wrapper").getBoundingClientRect().top < 0) {
@@ -7,7 +63,7 @@ setTimeout( () => {
     }
     else {
         document.getElementById("wrapper").className = "wrapperanimation"
-        setTimeout(() => {document.body.style ="overflow-y: visible;"}, 4900)
+        setTimeout(() => {document.body.style ="overflow-y: visible;"; allowMobileScroll = true}, 4900)
     }
 }, 50)
 
@@ -44,3 +100,20 @@ function changeclass(id, newclass) {
 
     element.className = newclass
 }
+
+document.body.addEventListener('touchstart', function(e) {
+    if (e.target.nodeName === 'INPUT' && !allowMobileScroll) {
+        e.preventDefault()
+    }
+});
+
+var elementid = 'writingtext'
+
+var element = document.getElementById(elementid)
+
+function inview() {
+    // this gets called if the element is in view
+   element.classList.add()
+}
+
+var myob = new Observer([new ObserverFunction(element, inview, ()=>{})], 10)
